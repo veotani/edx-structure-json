f07e482efcfb554551e672235353bf991b1a6b75
Anton Orlov <ion6431@gmail.com>
2020-03-06 02:49:20 +0300

    better design

--- parser.go
+++ parser.go
@@ -15,62 +15,41 @@ func ParseCourse() (Course, error) {
 	return course, err
 }
 
-// CourseXML is a description of CoursePath + "/course/course.xml" XML file
-type CourseXML struct {
-	DisplayName string       `xml:"display_name,attr"`
-	Chapters    []ChapterXML `xml:"chapter"`
-}
-
-// ChapterXML is a description of CoursePath + "/chapter/" + ChapterID + ".xml" XML file
-type ChapterXML struct {
-	URLName     string          `xml:"url_name,attr"`
-	DisplayName string          `xml:"display_name,attr"`
-	Sequentials []SequentialXML `xml:"sequential"`
-}
-
-// SequentialXML is a description of CoursePath + "/sequential/" + SequentialID + ".xml" XML file
-type SequentialXML struct {
-	URLName string `xml:"url_name,attr"`
-	// TODO: add CoursePath + "/sequential/" + SequentialID + ".xml" file structure
-}
-
 func parseCourseRoot() (Course, error) {
-	courseRootXMLReader, err := ioutil.ReadFile(CoursePath + "/course/course.xml")
+	course := &Course{}
+	err := parseByStructure(course, CoursePath+"/course/course.xml")
 	if err != nil {
 		return Course{}, err
 	}
+	for chapterNum, chapter := range course.Chapters {
+		err = parseByStructure(&course.Chapters[chapterNum], CoursePath+"/chapter/"+chapter.URLName+".xml")
+		if err != nil {
+			return Course{}, err
+		}
 
-	courseXML := CourseXML{}
-	err = xml.Unmarshal(courseRootXMLReader, &courseXML)
-	if err != nil {
-		return Course{}, err
+		fmt.Println(chapter)
+		// for _, sequential := range chapter.Sequentials {
+		// 	err = parseByStructure(&sequential, CoursePath+"/sequential/"+sequential.URLName+".xml")
+		// 	if err != nil {
+		// 		return Course{}, err
+		// 	}
+
+		// }
 	}
 
-	for _, chapter := range courseXML.Chapters {
-		parseChapter(chapter.URLName)
-	}
-
-	return Course{}, nil
+	return *course, err
 }
 
-func parseChapter(chapterURL string) (Chapter, error) {
-	chapterXMLReader, err := ioutil.ReadFile(CoursePath + "/chapter/" + chapterURL + ".xml")
+func parseByStructure(structureObject interface{}, filePath string) error {
+	fileToParse, err := ioutil.ReadFile(filePath)
 	if err != nil {
-		return Chapter{}, err
+		return err
 	}
 
-	chapterXML := ChapterXML{}
-	err = xml.Unmarshal(chapterXMLReader, &chapterXML)
-	fmt.Println(chapterXML)
+	err = xml.Unmarshal(fileToParse, structureObject)
 	if err != nil {
-		return Chapter{}, err
+		return err
 	}
 
-	for _, sequential := range chapterXML.Sequentials {
-		fmt.Println(sequential)
-		// Parse (CoursePath + "/sequential/" + SequentialID + ".xml")
-		// parseSequential(sequential)
-	}
-
-	return Chapter{}, nil
+	return nil
 }
