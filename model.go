3071e0d9954321b8b47f4ec445baf481285cd426
Anton Orlov <ion6431@gmail.com>
2020-03-06 02:47:44 +0300

    add xml tags; clean up a bit

--- model.go
+++ model.go
@@ -1,60 +1,79 @@
 package parser
 
+// Course is a description of CoursePath + "/course/course.xml" XML file
 type Course struct {
-	DisplayName string
-	ID          string
-	Chapters    []Chapter
+	DisplayName string    `xml:"display_name,attr"`
+	Chapters    []Chapter `xml:"chapter"`
 }
 
+// Chapter is a description of CoursePath + "/chapter/" + ChapterID + ".xml" XML file
 type Chapter struct {
-	DisplayName string
-	ID          string
-	Sequentials []Sequential
+	URLName     string       `xml:"url_name,attr"`
+	DisplayName string       `xml:"display_name,attr"`
+	Sequentials []Sequential `xml:"sequential"`
 }
 
+// Sequential is a description of CoursePath + "/sequential/" + SequentialID + ".xml" XML file
 type Sequential struct {
-	DisplayName string
-	ID          string
-	Verticals   []Vertical
+	URLName string `xml:"url_name,attr"`
+	// TODO: add CoursePath + "/sequential/" + SequentialID + ".xml" file structure
 }
 
-type Vertical struct {
-	DisplayName     string
-	ID              string
-	Problems        []Problem
-	Discussions     []Discussion
-	Htmls           []Html
-	OpenAssessments []OpenAssessment
-	LibraryContents []LibraryContent
-}
+// type Course struct {
+// 	DisplayName string
+// 	ID          string
+// 	Chapters    []Chapter
+// }
 
-type LibraryContent struct {
-	DisplayName string
-	ID          string
-	Problems    []Problem
-}
+// type Chapter struct {
+// 	DisplayName string
+// 	ID          string
+// 	Sequentials []Sequential
+// }
 
-type OpenAssessment struct {
-	DisplayName string
-	ID          string
-}
+// type Sequential struct {
+// 	DisplayName string
+// 	ID          string
+// 	Verticals   []Vertical
+// }
 
-type Html struct {
-	DisplayName string
-	ID          string
-}
+// type Vertical struct {
+// 	DisplayName     string
+// 	ID              string
+// 	Problems        []Problem
+// 	Discussions     []Discussion
+// 	Htmls           []Html
+// 	OpenAssessments []OpenAssessment
+// 	LibraryContents []LibraryContent
+// }
 
-type Video struct {
-	DisplayName string
-	ID          string
-}
+// type LibraryContent struct {
+// 	DisplayName string
+// 	ID          string
+// 	Problems    []Problem
+// }
 
-type Discussion struct {
-	ID          string
-	DisplayName string
-}
+// type OpenAssessment struct {
+// 	DisplayName string
+// 	ID          string
+// }
 
-type Problem struct {
-	ID          string
-	DisplayName string
-}
+// type Html struct {
+// 	DisplayName string
+// 	ID          string
+// }
+
+// type Video struct {
+// 	DisplayName string
+// 	ID          string
+// }
+
+// type Discussion struct {
+// 	ID          string
+// 	DisplayName string
+// }
+
+// type Problem struct {
+// 	ID          string
+// 	DisplayName string
+// }
